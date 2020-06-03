18b912f97ce804c783b1938ac5731496f763b2f0
limitlessv <13782917+limitlessv@users.noreply.github.com>
2020-06-03 06:47:47 -0700

    Initial commit

---
+++ bitnami-stack/nginx/opt/wp248/scripts/setup-path.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "# The next line updates PATH for the WP248 scripts." >> $HOME/.bash_profile
+echo "# added by wp248 script" >> $HOME/.bash_profile
+echo "if [ -f '/opt/wp248/scripts/path.bash.inc' ]; then . '/opt/wp248/scripts/path.bash.inc'; fi
